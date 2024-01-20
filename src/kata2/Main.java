bfe9b7435160d2eab48f8b1f395d4ffb864a9a7b
hunte <hunterofseries13@gmail.com>
2024-01-20 04:06:09 +0000

    first commit

---
+++ src/kata2/Main.java
@@ -0,0 +1,16 @@
+package kata2;
+
+import java.io.File;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        OrganizationLoader loader = new TsvFileOrganizationLoader(new File("dataset.tsv"));
+        List<Organization> organizations = loader.load();
+        Map<String, Integer> barchart = new CountryOrganizationprocessor().process(organizations);
+        for (String key : barchart.keySet()) {
+            System.out.println(key + " = " + barchart.get(key));
+        }
+    }
+}
